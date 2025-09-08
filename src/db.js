db81e80a1cdb562e5a3b7792de2e533874ea5ef9
Ayax18ing <samuelzuniga@unicauca.edu.co>
2025-09-08 20:40:53 +0000

    Laboratorio Backend

--- src/db.js
+++ src/db.js
@@ -3,12 +3,12 @@ import mongoose from "mongoose";
 
 
 const DB_USER = "admin"; // Usuario de MongoDB
-const DB_PASSWORD = "vagrant"; // Contraseña del usuario
-const DB_HOST = "selene.unicauca.edu.co"; // O IP del servidor MongoDB
-const DB_PORT = "8004"; // Puerto en el que corre MongoDB
-const DB_NAME = "appdb"; // Nombre de la base de datos
+const DB_PASSWORD = "password123"; // Contraseña del usuario
+const DB_HOST = "192.168.4.85"; // O IP del servidor MongoDB
+const DB_PORT = "27017"; // Puerto en el que corre MongoDB
+const DB_NAME = "valen"; // Nombre de la base de datos
 
-const mongoURI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
+const mongoURI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=valen`;
 export const connectDB = async () => {
     try {
         await mongoose.connect(mongoURI);
