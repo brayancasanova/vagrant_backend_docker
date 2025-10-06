c9ecb3fe32dfff0d67b7ade8d522db53c50d63c4
brayancasanova <bcasanova262@gmail.com>
2025-10-06 13:46:14 +0000

    primer commit backend docker

--- src/db.js
+++ src/db.js
@@ -4,11 +4,11 @@ import mongoose from "mongoose";
 
 const DB_USER = "admin"; // Usuario de MongoDB
 const DB_PASSWORD = "password123"; // Contraseña del usuario
-const DB_HOST = "10.132.15.134"; // O IP del servidor MongoDB
+const DB_HOST = "mongo"; // O IP del servidor MongoDB
 const DB_PORT = "27017"; // Puerto en el que corre MongoDB
 const DB_NAME = "valen"; // Nombre de la base de datos
 
-const mongoURI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=valen`;
+const mongoURI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
 export const connectDB = async () => {
     try {
         await mongoose.connect(mongoURI);
